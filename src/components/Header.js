fe95483828aa1ca54df803d798731738c76d902e
Stephen Lenane <info@stephenlenane.com>
2022-05-09 20:39:04 +0100

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,36 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../store/auth';
+
 import classes from './Header.module.css';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  const logout = () => {
+    dispatch(authActions.logout());
+  };
+
+  const authSection = (
+    <nav>
+      <ul>
+        <li>
+          <a href='/'>My Products</a>
+        </li>
+        <li>
+          <a href='/'>My Sales</a>
+        </li>
+        <li>
+          <button onClick={logout}>Logout</button>
+        </li>
+      </ul>
+    </nav>
+  );
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {isAuthenticated && authSection}
     </header>
   );
 };
